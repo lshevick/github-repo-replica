44653fdfc10dda5466735d33e6b623516cb1a272
Levi Shevick <levidshevick@gmail.com>
2022-05-25 17:51:10 -0400

    adds basic structure and navs

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,47 @@
 import { token } from '../modules/auth_token.js';
 
 const BASE_URL = 'https://api.github.com/users/lshevick';
+const headers = {
+    'User_Agent': 'lshevick',
+    'Accept': 'application/vnd.github.v3+json',
+    'Authorization': `token ${token}`,
+};
+
+const generateRepos = (data) => {
+    const source = document.querySelector('#repo-template').innerHTML;
+    const template = Handlebars.compile(source);
+    const context = {repos: data};
+    const html = template(context);
+    console.log(context);
+    document.querySelector('.repo-wrapper').innerHTML = html;
+}
+
+const generateProfile = (data) => {
+    const source = document.querySelector('#profile-template').innerHTML;
+    const template = Handlebars.compile(source);
+    const context = {profile: data};
+    const html = template(context);
+    console.log(context);
+    document.querySelector('.profile-wrapper').innerHTML = html;
+}
+
+fetch(`${BASE_URL}/repos`, headers)
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response bad');
+    } else {
+        return response.json();
+    }
+})
+.then(data => generateRepos(data));
+
+
+fetch(`${BASE_URL}`, headers)
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response bad');
+    } else {
+        return response.json();
+    }
+})
+.then(data => generateProfile(data));
