05a9b88895ac95154f75e7a522a7530ff1c8a8fc
Levi Shevick <levidshevick@gmail.com>
2022-05-25 23:06:04 -0400

    adds buttons and repo info

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 import { token } from '../modules/auth_token.js';
+// import { formatDistanceToNow } from '../date-fns';
 
 const BASE_URL = 'https://api.github.com/users/lshevick';
 const headers = {
@@ -7,10 +8,34 @@ const headers = {
     'Authorization': `token ${token}`,
 };
 
+// Handlebars.registerHelper('formatDate', function (dateString) {
+//     return new Handlebars.safeString(
+//         formatDistanceToNow(dateString)
+//     );
+// });
+
+const updateLanguageColor = () => {
+    const langColor = document.querySelector('.repo-lang-color');
+    const lang = document.querySelector('.repo-language');
+
+    switch (lang.innerHTML) {
+        case 'HTML':
+            langColor.style.backgroundColor = '#D45635';
+            break;
+        case 'CSS':
+            langColor.style.backgroundColor = '#524078';
+            break;
+        case 'Javascript':
+            langColor.style.backgroundColor = '#EFDF70';
+            break;
+    }
+}
+
+
 const generateRepos = (data) => {
     const source = document.querySelector('#repo-template').innerHTML;
     const template = Handlebars.compile(source);
-    const context = {repos: data};
+    const context = { repos: data };
     const html = template(context);
     console.log(context);
     document.querySelector('.repo-wrapper').innerHTML = html;
@@ -19,29 +44,33 @@ const generateRepos = (data) => {
 const generateProfile = (data) => {
     const source = document.querySelector('#profile-template').innerHTML;
     const template = Handlebars.compile(source);
-    const context = {profile: data};
+    const context = { profile: data };
     const html = template(context);
     console.log(context);
     document.querySelector('.profile-wrapper').innerHTML = html;
 }
 
 fetch(`${BASE_URL}/repos`, headers)
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response bad');
-    } else {
-        return response.json();
-    }
-})
-.then(data => generateRepos(data));
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response bad');
+        } else {
+            return response.json();
+        }
+    })
+    .then(data => generateRepos(data));
 
 
 fetch(`${BASE_URL}`, headers)
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response bad');
-    } else {
-        return response.json();
-    }
-})
-.then(data => generateProfile(data));
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response bad');
+        } else {
+            return response.json();
+        }
+    })
+    .then(data => generateProfile(data));
+
+
+
+    updateLanguageColor();
